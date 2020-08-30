6f8ee1f7ee346e890403522f99f5c1350904b717
Joost van Schaik <joostvanschaik@outlook.com>
2020-08-30 14:47:53 +0200

    Updated Service Profile

--- Assets/Application/Scripts/Services/DemoServiceProfile.cs
+++ Assets/Application/Scripts/Services/DemoServiceProfile.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit;
+using MRTKExtensions.ServiceExtensions;
 
 namespace DemoApp.Services
 {
@@ -8,6 +9,10 @@ namespace DemoApp.Services
 	[CreateAssetMenu(fileName = "DemoServiceProfile", menuName = "MixedRealityToolkit/DemoService Configuration Profile")]
 	public class DemoServiceProfile : BaseMixedRealityProfile
 	{
-		// Store config data in serialized fields
+        [SerializeField] private string someProperty = "test";
+        public string SomeProperty => someProperty;
+
+        [SerializeField] private bool someOtherProperty = true;
+        public bool SomeOtherProperty => someOtherProperty;
 	}
 }
